3f4ca9fa23e3981bc4ca76fa5492e9054a1a786d
Julia <jukuznetswork@gmail.com>
2023-02-10 16:19:37 +0100

    [f] database schema

--- aviasales-webservice/database/schema.sql
+++ aviasales-webservice/database/schema.sql
@@ -34,77 +34,32 @@ CREATE TABLE containers (
     adult_price FLOAT NULL,
     child_price FLOAT NULL,
     infant_price FLOAT NULL,
-    currency_id INTEGER NULL,
-    request_time TIMESTAMP NOT NULL,
-
-    CONSTRAINT fk_currency
-        FOREIGN KEY(currency_id)
-        REFERENCES currencies(id)
-        ON DELETE SET NULL
+    currency_id INTEGER NULL REFERENCES currencies(id) ON DELETE SET NULL,
+    request_time TIMESTAMP NOT NULL
 );
 
 CREATE TABLE itineraries (
     id SERIAL PRIMARY KEY,
     itinerary_count INTEGER UNIQUE NOT NULL,
-    container_count_id INTEGER NOT NULL,
+    container_count_id INTEGER NOT NULL REFERENCES containers(container_count) ON DELETE SET NULL,
     total_flight_time INTEGER NOT NULL,
-    priced_itinerary_id INTEGER NOT NULL,
-
-    CONSTRAINT fk_container_count
-        FOREIGN KEY(container_count_id)
-        REFERENCES containers(container_count)
-        ON DELETE SET NULL,
-
-    CONSTRAINT fk_priced_itinerary
-        FOREIGN KEY(priced_itinerary_id)
-            REFERENCES priced_itineraries(id)
-            ON DELETE SET NULL
+    priced_itinerary_id INTEGER NOT NULL REFERENCES priced_itineraries(id) ON DELETE SET NULL
 );
 
 CREATE TABLE flights (
     id SERIAL PRIMARY KEY,
-    itinerary_count_id INTEGER NOT NULL,
+    itinerary_count_id INTEGER NOT NULL REFERENCES itineraries(itinerary_count) ON DELETE SET NULL,
     flight_part INTEGER NOT NULL,
     direct_flight BOOLEAN NOT NULL,
-    carrier_id INTEGER NULL,
+    carrier_id INTEGER NULL REFERENCES carriers(id) ON DELETE SET NULL,
     flight_num INTEGER NULL,
-    source_id INTEGER NULL,
-    destination_id INTEGER NULL,
+    source_id INTEGER NULL REFERENCES cities(id) ON DELETE SET NULL,
+    destination_id INTEGER NULL REFERENCES cities(id) ON DELETE SET NULL,
     departure_time TIMESTAMP NULL,
     arrival_time TIMESTAMP NULL,
     flight_time INTEGER NULL,
-    class_field_id INTEGER NULL,
+    class_field_id INTEGER NULL REFERENCES classes(id) ON DELETE SET NULL,
     num_stops INTEGER NULL,
-    ticket_type_id INTEGER NULL,
-    created TIMESTAMP NOT NULL,
-
-    CONSTRAINT fk_itinerary_count
-        FOREIGN KEY(itinerary_count_id)
-            REFERENCES itineraries(itinerary_count)
-            ON DELETE SET NULL,
-
-    CONSTRAINT fk_carrier
-        FOREIGN KEY(carrier_id)
-            REFERENCES carriers(id)
-            ON DELETE SET NULL,
-
-    CONSTRAINT fk_source
-        FOREIGN KEY(source_id)
-            REFERENCES cities(id)
-            ON DELETE SET NULL,
-
-    CONSTRAINT fk_destination
-        FOREIGN KEY(destination_id)
-            REFERENCES cities(id)
-            ON DELETE SET NULL,
-
-    CONSTRAINT fk_class_field
-        FOREIGN KEY(class_field_id)
-            REFERENCES classes(id)
-            ON DELETE SET NULL,
-
-    CONSTRAINT fk_ticket_type
-        FOREIGN KEY(ticket_type_id)
-            REFERENCES ticket_types(id)
-            ON DELETE SET NULL
+    ticket_type_id INTEGER NULL REFERENCES ticket_types(id) ON DELETE SET NULL,
+    created TIMESTAMP NOT NULL
 );
